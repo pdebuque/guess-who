1e4dd1b4ac6f4767596e1e497747209968db5229
Paolo Debuque <pdebuque@gmail.com>
2022-10-31 20:07:26 -0500

    added nearly all functionality. need to clean up code and make one simple reset game function

--- client.js
+++ client.js
@@ -5,11 +5,49 @@ console.log('Here are all the available people:', people); // i added two so it 
 $(document).ready(onReady);
 
 function onReady() {
-    loadImages();
+    loadRandom();
     $('#play-game-btn').on('click', startGame);
 }
 
 
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min)
+}
+
+// function to create a randomized array of length 9 (could fairly easily be generalized to larger arrays)
+function shuffleArr9() {
+    const vanillaArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const randomArr = [];
+
+    for (i = 0; i < 9; i++) {
+        const iterationRand = randomNum(0, 8 - i); //array will decrease in length each time
+        randomArr[i] = vanillaArr[iterationRand];
+        vanillaArr.splice(iterationRand, 1);
+    }
+    return randomArr;
+}
+
+function loadRandom() {
+    // create a randomized array 0-8
+
+    const indexArr = shuffleArr9();
+    console.log(indexArr);
+
+    $('#list-of-people').empty();
+    // for loop: take random array value and use it as index to choose which person
+    for (let i = 0; i < 9; i++) {
+        const personToAdd = people[indexArr[i]];
+        $('#list-of-people').append(`
+            <div>
+                <img class="prof-pic" id="${personToAdd.name}" src="https://github.com/${personToAdd.githubUsername}.png?size=250" alt = "Profile image of ${personToAdd.name}">
+                <p class="on-image-text"></p>
+            </div>
+        `)
+    }
+
+
+    // append people
+}
 
 function loadImages() {
     for (let person of people) {
@@ -22,8 +60,6 @@ function loadImages() {
     }
 }
 
-
-
 function startGame() {
     console.log('in startGame');
     // reset stuff in case this is not the first time playing
@@ -31,11 +67,15 @@ function startGame() {
     $('#play-game-btn').addClass('btn-in-game');
     $('#play-game-btn').empty().text('MAKE YOUR GUESS');
 
+    $('img').removeClass('blur');
+    $('img').next().empty();
+
     // how to randomize array of images?...
 
+
     // choose random integer between 1 and the number of people
-    const randomNum = Math.ceil(Math.random() * people.length);
-    let personToGuess = people[randomNum].name;
+    const personInd = randomNum(0, 8)
+    let personToGuess = people[personInd].name;
 
     $('img').addClass('img-game-on')
 
@@ -88,4 +128,8 @@ function tryAgain() {
     $(this).next().empty().append(`
         Nope! Try again!
     `)
+}
+
+function resetGame() {
+
 }
